84290346b751e6bcc559ba0b4c89d5b060d4e4a2
rffantin <rffantin@gmail.com>
2023-10-11 20:23:21 -0300

    feat: :sparkles: Criação da base da lista de tarefas
    Base para inserção de tarefas criada

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,25 @@
+import { useState } from 'react'
+import './App.css'
 
+function App() {
+const [toDos, setToDos] = useState([])
+
+const [inputValue, setInputValue] = useState("")
+function handleOnChange(e){
+setInputValue(e.target.value)
+console.log(inputValue)
+}
+  return ( 
+
+    <>   
+    <ul>
+    {toDos.map((tarefa, index) => <li key={index}>{tarefa}</li>)}
+    </ul>
+    <input onChange={handleOnChange} value={inputValue} type="text" />
+    <button onClick={() => setToDos([...toDos, inputValue])}>Adicionar tarefa</button>
+    </>
+  
+  )
+}
+
+export default App
